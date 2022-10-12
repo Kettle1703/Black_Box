ad512491933053ac6679f1e22a2c503a3e9e63f8
atnemelc <102025370+atnemelc@users.noreply.github.com>
2022-10-12 20:10:11 +0500

    проект

---
+++ проект/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace проект
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
