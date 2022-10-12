3f032f62f2f226a5abf35adfdf5e2a5fd9562043
Kettle1703 <prytkovmisha@yandex.ru>
2022-10-12 18:35:04 +0300

    C# lab 2
    C# lab 2

---
+++ C_Lab_2/C_Lab_2/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Lab_2
+{
+    internal class Point  // модификатор internal делает, так что класса доступен из любого места кода в той же сборке
+    {                    // класс описывает точки в двухмерном пространстве
+        private double x;  // координата точки по оси Абцисс
+        private double y;  // координата точки по оси Ординат
+        private char name;  // название точки, нужно для того чтобы их различать
+
+        public Point(double x, double y, char name)  // конструктор копирования
+        {
+            this.x = x;
+            this.y = y;
+            this.name = name;
+        }
+
+        public Point()  // конструктор, заполняющий поля случайным образом
+        {
+            Random random = new Random();
+            this.x = random.Next(-10, 10) + Math.Round(random.NextDouble(), 1);
+            this.y = random.Next(-10, 10) + Math.Round(random.NextDouble(), 1);
+            this.name = (char)random.Next(65, 90);
+        }
+        
+        public char Name
+        {
+            get { return name; }
+        }
+
+        public void Get_info()  // метод, выводящий все поля объекта в консоль
+        {
+            Console.WriteLine($"Точка {this.name}:\n{this.x}\n{this.y}\n");
+        }
+
+        override public string ToString()  // переопределяем существующий метод ToString, при помощи модификатора override
+        {                                 //метод возвращающий стороку, в которой содержатся все поля класса
+            return (this.x + "  " + this.y + "  " + this.name);
+        }
+
+        public double Remoteness()  // расстояние от точки до центра координат
+        {                          // Remoteness - удалённость
+            return Math.Round(Math.Sqrt(this.x * this.x + this.y * this.y), 3);
+        }
+
+        public static Point operator --(Point point1)  // перегрузка унарного оператора для нашего класса
+                                                      // применив на объект операцию -- из полей координат вычтется по единице
+        {
+            point1.x--;
+            point1.y--;
+            return point1;
+        }
+
+        public static Point operator -(Point point1)  // перегрузка унарного оператора -
+        {                                            // поля координат объкта меняются местами
+            double x_save = point1.x;
+            point1.x = point1.y;
+            point1.y = x_save;
+            return point1;
+        }
+
+        public static implicit operator int(Point point1)  // Перегрузка операций преобразования типов. Модификатор implicit отвечает за неявное преобразование типов
+        {                                                  // Если объект присваивается перменной типа int, то берётся поле X и присваивается перемнной int
+            return (int)point1.x;  // явное преобразование типов, так как изначально поля типа double
+        }
+
+        public static explicit operator double(Point point1)  // Перегрузка операций преобразования типов. Модификатор explicit отвечает за явное преобразование типов ( когда перед переменной в скобках указазываем новый тип )
+        {                                                     // Если объект присваивается перменной типа double, то берётся поле Y и присваивается перемнной double
+            return point1.y;
+        }
+
+        public static Point operator -(Point point1, int number)  // перегрузка бинарног оператора вычитания.
+        {                                                        // когда из точки вычитается целое число, то уменьшается Х координата на это число
+            point1.x -= number;
+            return point1;
+        }
+
+        public static Point operator -(int number, Point point1)  // перегрузка бинарног оператора вычитания.
+        {                                                        // когда из целого числа вычитается точка, то уменьшается Y координата на это число         
+            point1.y -= number;
+            return point1;
+        }
+
+        public static double operator -(Point first, Point second)  // перегрузка бинарног оператора вычитания. 
+        {                                                          // при вычитании одного объекта из другого, то есть одной точки из другой, между ними считается расстояние   
+            Console.Write($"Расстояние между точками {first.Name} и {second.Name}: ");
+            double result = Math.Pow(first.x - second.x, 2) + Math.Pow(first.y - second.y, 2);
+            return Math.Round(Math.Sqrt(result), 3);
+        }
+    }
+}
---
+++ C_Lab_2/C_Lab_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace C_Lab_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Point point_1 = new Point();
+            Point point_2 = new Point(10, 1.4, 'F');
+            Point point_3 = new Point(-1, -1, 'E');
+            point_1.Get_info();
+            point_2.Get_info();
+            int number = point_1;
+            //char number_3 = point_1;
+            double number_2 = (double)point_1;
+            Console.WriteLine($" int {number} double {number_2}");
+            Console.WriteLine($"Получение полей объектов строками:\n{point_1.ToString()}   {point_2.ToString()}\n");
+            Console.WriteLine($"Удалённость точки {point_1.Name} {point_1.Remoteness()}\n");
+            /*
+            point_1 = point_1 - 10;
+            point_1.Get_info();
+            point_1 = 10 - point_1;
+            point_1.Get_info();
+            */
+            //point_2--;
+            //Console.WriteLine(-point_2);
+            //point_2.Get_info();
+            
+            //Console.WriteLine(point_2 - point_3);
+            //Операции приведения типа: в переменную целого типа записывается x
+            //                          в переменную вещественного типа записывается y
+            /*
+            Бинарные операции:
+            – Point p, целое число(левосторонняя операция,
+            уменьшается координата х);  
+            
+            point2 - 13 и 13 вычитается из поля х
+            
+            
+            – целое число, Point p(правосторонняя операция,
+            уменьшается координата y);  
+            
+            13 - point2 и 13 вычитается из поля у
+            
+           
+            – Point p – вычисляется расстояние до точки p, результатом
+            должно быть вещественное число.  
+            
+            point1 + point2  расстояние между точками (оператор можно поменять)
+            */
+        }
+    }
+}
