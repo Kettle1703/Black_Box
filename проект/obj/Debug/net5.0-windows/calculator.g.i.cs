752c41d56390afc344cfe59c7090355b6562f5a7
Kettle1703 <prytkovmisha@yandex.ru>
2022-11-12 22:51:50 +0300

    Изменения алгоритмов

--- проект/obj/Debug/net5.0-windows/calculator.g.i.cs
+++ проект/obj/Debug/net5.0-windows/calculator.g.i.cs
@@ -95,13 +95,13 @@ namespace проект {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/проект;component/calculator.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/проект;V1.0.0.0;component/calculator.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\calculator.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -111,7 +111,7 @@ namespace проект {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
